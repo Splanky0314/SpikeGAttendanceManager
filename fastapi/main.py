d80e9f5d3188c9bc52e306b716b19e21fa8de4c0
splanky0314 <splanky0314@gmail.com>
2024-08-15 04:29:56 +0900

    fastapi | FEAT: create attendance code & save attendance code to spread sheet

--- fastapi/main.py
+++ fastapi/main.py
@@ -29,3 +29,38 @@ app = FastAPI()
 @app.get("/")
 def test():
     pass
+
+@app.get("/create_attendance_code")
+def create_attendance_code():
+    create_attcode()
+    return {"success":"true", "message":"Please check the spread sheet to verify if the Attendance Code has been generated successfully."}
+
+def create_randomstr(length = 5):
+    characters = string.ascii_uppercase + string.digits
+    attcode = ''.join(random.choices(characters, k=length))
+    return attcode
+
+def create_attcode():
+    i = 4
+    while True :
+        i += 1
+        # 연습 일정이 존재하고
+        if ws_schedule.acell('A'+str(i)).value:
+            print(ws_schedule.acell('A'+str(i)).value)
+            # 이미 출결코드가 생성되어 있다면
+            if ws_schedule.acell('B'+str(i)).value:
+                continue
+            # 출결코드가 생성되지 않았다면
+            else:
+                ws_schedule.update_acell('B'+str(i), create_randomstr())
+                ws_schedule.update_acell('C'+str(i), create_randomstr())
+        else:
+            break
+
+@app.post("/check_stdnumber")
+def check_stdnumber():
+    pass
+
+@app.post("/check_attcode")
+def check_attcode():
+    pass
